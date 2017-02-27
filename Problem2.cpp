e8dfc750d505f837efdc8bcb65af4a64b7076d81
shubham raj <shubhamraj97@gmail.com>
2017-02-27 12:49:13 +0530

    Update and rename Problem2 to Problem2.cpp

---
+++ Problem2.cpp
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	int height,weight;
+	string bad[100];
+	cout<<"enter the width"<<endl;
+	cin>>width;
+	cout<<"enter the length"<<endl;
+	cin>>height;
+	cout<<"enter the bad array"<<endl;
+	return 0;
+}
